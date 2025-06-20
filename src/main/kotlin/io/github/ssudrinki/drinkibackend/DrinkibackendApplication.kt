ad8f1992c483d73471c98a9822b06e342e2c3fde
leemooyoung <hokokok37@gmail.com>
2025-06-20 17:12:49 +0900

    init

---
+++ src/main/kotlin/io/github/ssudrinki/drinkibackend/DrinkibackendApplication.kt
@@ -0,0 +1,11 @@
+package io.github.ssudrinki.drinkibackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DrinkibackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DrinkibackendApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/ssudrinki/drinkibackend/DrinkibackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.ssudrinki.drinkibackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DrinkibackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
