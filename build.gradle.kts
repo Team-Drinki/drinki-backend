a771ac016652c374d45866a894036034b4d83220
leemooyoung <leemoo1004@gmail.com>
2025-07-27 17:32:51 +0900

    feat: manage dependency versions with libs.versions.toml
    and add exposed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.5.0"
-	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "1.9.25"
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
 }
 
 group = "io.github.ssudrinki"
@@ -20,15 +19,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(libs.kotlin.reflect)
+	implementation(libs.spring.boot.starter.web)
+	testImplementation(libs.spring.boot.starter.test)
+	developmentOnly(libs.spring.boot.devtools)
+	testImplementation(libs.kotlin.test.junit5)
+	testRuntimeOnly(libs.junit.platform.launcher)
+	implementation(libs.exposed.spring.boot.starter)
+	implementation(libs.jackson.module.kotlin)
+	runtimeOnly(libs.h2)
 }
 
 kotlin {
