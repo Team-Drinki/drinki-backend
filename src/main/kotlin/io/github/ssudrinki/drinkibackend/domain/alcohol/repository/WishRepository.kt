79d05c1dd750f17f1e4ea966d4fd27b27f644c7a
Ahnjaemin1208 <michael1208@naver.com>
2025-09-02 22:09:49 +0900

    feat : Wish Repository Layer Logic
    술 위시 관련 repository 로직 구현

--- src/main/kotlin/io/github/ssudrinki/drinkibackend/domain/alcohol/repository/WishRepository.kt
+++ src/main/kotlin/io/github/ssudrinki/drinkibackend/domain/alcohol/repository/WishRepository.kt
@@ -1,5 +1,7 @@
 package io.github.ssudrinki.drinkibackend.domain.alcohol.repository
 
+import io.github.ssudrinki.drinkibackend.domain.alcohol.entity.WishDto
+
 /**
  * 술 위시 정보를 관리하는 Repository 인터페이스
  *
@@ -7,14 +9,36 @@ package io.github.ssudrinki.drinkibackend.domain.alcohol.repository
  * 검색, 필터링 기능을 제공합니다.
  */
 interface WishRepository {
+    /**
+     * 사용자의 술 위시 리스트를 조회합니다
+     *
+     * @param userId         위시리스트 조회할 사용자 ID
+     * @return List(WishDto) 위시리스트
+     */
+    fun findByUserId(userId: Long): List<WishDto>
 
-    fun findByUserId()
+    /**
+     * 사용자의 술 위시 정보를 조회합니다.
+     *
+     * @param userId    사용자 ID
+     * @param alcoholId 위시 조회할 술 ID
+     * @return WishDto? 위시
+     */
+    fun findByUserIdAndAlcoholId(userId: Long, alcoholId: Int): WishDto?
 
-    fun findByUserIdAndAlcoholId(userId: Int, alcoholId: Int)
+    /**
+     * 사용자의 술 위시를 추가합니다.
+     *
+     * @param userId 사용자 ID
+     * @param alcoholId 추가할 술 ID
+     */
+    fun create(userId: Long, alcoholId: Int)
 
-    fun create()
-
-    fun delete()
-
-
+    /**
+     * 사용자의 술 위시를 삭제합니다.
+     *
+     * @param userId 사용자 ID
+     * @param alcoholId 삭제할 술 ID
+     */
+    fun delete(userId: Long, alcoholId: Int)
 }
--- src/main/kotlin/io/github/ssudrinki/drinkibackend/domain/alcohol/repository/WishRepositoryImpl.kt
+++ src/main/kotlin/io/github/ssudrinki/drinkibackend/domain/alcohol/repository/WishRepositoryImpl.kt
@@ -1,14 +1,70 @@
 package io.github.ssudrinki.drinkibackend.domain.alcohol.repository
 
+import io.github.ssudrinki.drinkibackend.domain.alcohol.entity.WishDto
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Repository
+import schema.Users
+import schema.Wishes
 
 @Repository
 class WishRepositoryImpl : WishRepository {
-    override fun findByUserIdAndAlcoholId(): AlcoholWishResponse {
-//        TODO("Not yet implemented")
+    override fun findByUserId(userId: Long): List<WishDto> {
+        return transaction {
+            Wishes
+                    .selectAll()
+                    .where{
+                        (Wishes.userId eq userId)
+                    }
+                    .map { row ->
+                        WishDto(
+                                id = row[Wishes.id],
+                                userId = row[Wishes.userId],
+                                alcoholId = row[Wishes.alcoholId],
+                                createdAt = row[Wishes.createdAt]
+                        )
+                    }
+        }
     }
 
-    override fun findById(id: Int): AlcoholWishResponse {
-//        TODO("Not yet implemented")
+    override fun findByUserIdAndAlcoholId(userId: Long, alcoholId: Int): WishDto? {
+        return transaction {
+            Wishes
+                    .join(Users, JoinType.LEFT, Wishes.userId, Users.id)
+                    .selectAll()
+                    .where {
+                        (Wishes.userId eq userId) and (Wishes.alcoholId eq alcoholId)
+                    }
+                    .map { row ->
+                        WishDto(
+                                id = row[Wishes.id],
+                                userId = row[Wishes.userId],
+                                alcoholId = row[Wishes.alcoholId],
+                                createdAt = row[Wishes.createdAt]
+                        )
+                    }.singleOrNull()
+        }
+    }
+
+    override fun create(userId: Long, alcoholId: Int) {
+        transaction {
+            Wishes.insert {
+                it[Wishes.userId] = userId
+                it[Wishes.alcoholId] = alcoholId
+                it[createdAt] = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+            }
+        }
+    }
+
+    override fun delete(userId: Long, alcoholId: Int) {
+        transaction {
+            Wishes.deleteWhere {
+                (Wishes.userId eq userId) and (Wishes.alcoholId eq alcoholId)
+            }
+        }
     }
 }
