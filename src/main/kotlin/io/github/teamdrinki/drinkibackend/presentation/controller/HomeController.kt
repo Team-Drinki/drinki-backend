0c807376ea7c3d1bfcf515236405ea94bf7e3f04
Lee Yeseul <yes0112@naver.com>
2025-10-05 15:10:20 +0900

    fix: add home api

---
+++ src/main/kotlin/io/github/teamdrinki/drinkibackend/presentation/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package io.github.teamdrinki.drinkibackend.presentation.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping("/", produces = ["text/plain"])
+    fun home(): String {
+        return "Hello, Welcome to Drinki Backend!"
+    }
+}
