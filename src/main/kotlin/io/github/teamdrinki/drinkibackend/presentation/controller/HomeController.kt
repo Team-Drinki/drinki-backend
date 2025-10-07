ba948f5cd33addfe31946aedbf29854ced49a5db
Lee Yeseul <yes0112@naver.com>
2025-10-07 21:18:18 +0900

    docs: add comment to home controller

--- src/main/kotlin/io/github/teamdrinki/drinkibackend/presentation/controller/HomeController.kt
+++ src/main/kotlin/io/github/teamdrinki/drinkibackend/presentation/controller/HomeController.kt
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HomeController {
 
+    /**
+     * 루트 페이지 요청을 처리하는 API 엔드포인트입니다.
+     *
+     * @return Hello, Welcome to Drinki Backend!
+     */
     @GetMapping("/")
     fun home(): String {
         return "Hello, Welcome to Drinki Backend!"
